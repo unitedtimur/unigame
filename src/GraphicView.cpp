5fb54b5d9c7325a5fa6ff1507e19f2a29a2c4a67
Timur Likhomanov <lihomanov.t@gmail.com>
2020-03-15 20:03:04 +0700

    init level 1

--- src/GraphicView.cpp
+++ src/GraphicView.cpp
@@ -1,6 +1,19 @@
 #include "include/GraphicView.h"
 
+#include <QMouseEvent>
+
 void GraphicView::mousePressEvent(QMouseEvent* event)
 {
-	emit this->mouseClicked(event);
+	if (event->button() == Qt::MouseButton::LeftButton)
+	{
+		emit this->mouseClicked(event);
+	}
+}
+
+void GraphicView::mouseMoveEvent(QMouseEvent* event)
+{
+	//if (event->button() == Qt::MouseButton::LeftButton)
+	//{
+	//	emit this->mouseClicked(event);
+	//}
 }
