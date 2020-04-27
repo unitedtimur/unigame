d62306afdaabaf643a4ba2fb4271c536120594b0
Timur Likhomanov <lihomanov.t@gmail.com>
2020-04-27 21:40:30 +0700

    release

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,10 @@
 #include <QApplication>
 
-#include "include/UniMainWindow.h"
 #include "include/Matrix.h"
-
-#include <QtCore/QTextCodec>
-#include <QTextCodec>
 
 int main(int argc, char** argv)
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
 
 	QApplication app(argc, argv);
 
